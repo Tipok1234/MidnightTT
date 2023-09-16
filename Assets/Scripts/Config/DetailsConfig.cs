4ea97301bc9fc139a905ca65aafb85b241e32252
tipok1234 <dan.grigulevich@gmail.com>
2023-09-16 15:50:21 +0300

    fix

--- Assets/Scripts/Config/DetailsConfig.cs
+++ Assets/Scripts/Config/DetailsConfig.cs
@@ -9,7 +9,6 @@ public class DetailsConfig : ScriptableObject
 {
     public List<Detail> details = new List<Detail>();
 
-
     public Detail GetDetail(string key)
     {
         return details.FirstOrDefault(d => d != null && d.key == key);
--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -7,7 +7,6 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
-    [SerializeField] private PlayerController carModel;
     [SerializeField] private DetailsConfig config;
     public PlayerController Car { get; private set; }
     public DetailsConfig Config => config;
