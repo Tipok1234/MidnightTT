b817ca4122c159b8e52c8bb5e77dad1255f11167
tipok1234 <dan.grigulevich@gmail.com>
2023-09-16 22:14:23 +0300

    add base logic for server

--- Assets/Scripts/UI/StartScreen.cs
+++ Assets/Scripts/UI/StartScreen.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 using System;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
 
-public class StartScreen : MonoBehaviour
+public class StartScreen : MonoBehaviourPunCallbacks
 {
     //public static event Action StartGameAction;
     public static event Action OpenShopScreenAction;
 
     [SerializeField] private Button startGameButton;
     [SerializeField] private Button shopScreenButton;
+    [SerializeField] private Button serverScreenButton;
 
     [SerializeField] private Canvas canvas;
+
+    [SerializeField] private GameObject serverPanel;
+
+    [Header("Setting Server Room")]
+    [SerializeField] private Button createRoom;
+    [SerializeField] private Button joinRoom;
+    [SerializeField] private Button exitButton;
+
+    [SerializeField] private InputField createField;
+    [SerializeField] private InputField joinField;
 
     private void Awake()
     {
         startGameButton.onClick.AddListener(StartGame);
         shopScreenButton.onClick.AddListener(OpenShopScreen);
+        serverScreenButton.onClick.AddListener(ConnectServer);
     }
 
     private void Start()
@@ -29,7 +42,6 @@ public class StartScreen : MonoBehaviour
     private void OnDestroy()
     {
         ShopScreen.ExitShopScreenAction -= ActiveScreen;
-
     }
 
     private void StartGame()
@@ -47,5 +59,29 @@ public class StartScreen : MonoBehaviour
     private void ActiveScreen(bool active)
     {
         canvas.enabled = active;
+    }
+
+
+    private void ConnectServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.LogError("Load");
+
+        serverPanel.SetActive(true);
+    }
+
+
+    public void CreateRoom()
+    {
+
+    }
+
+    public void JoinRoom()
+    {
+
     }
 }
